096c5f36d0d0fca1471e09181076cf2b36139331
Justin Makeig <jmpublic@makeig.com>
2026-07-03 17:29:44 +0000

    Adds /event route to test end-to-end

---
+++ src/routes/event/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import FormControl from '$components/FormControl/FormControl.svelte';
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
+<h1>Event</h1>
+
+<form>
+	<FormControl name="customer-workload"></FormControl>
+	<FormControl name="outcome">
+		{#snippet input({ name })}
+			<textarea {name}></textarea>
+		{/snippet}
+	</FormControl>
+	<FormControl
+		name="happened_at"
+		value={'data?.happened_at ?? new Date().toISOString().slice(0, 10)'}
+	></FormControl>
+</form>
