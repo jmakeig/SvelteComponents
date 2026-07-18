f0b6ba61907afc9d04f415bcd8bf52c04ea2aaad
Justin Makeig <jmpublic@makeig.com>
2026-07-18 18:20:26 +0000

    Better differentiates between user and framework/platform stack frames

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
 	import { page } from '$app/state';
+
+	const INTERNAL = /node_modules|node:internal/;
 </script>
 
 <h1>{page.error?.original?.name}: {page.error?.message} ({page.status})</h1>
@@ -8,7 +10,7 @@
 	<ol class="stack">
 		{#each page.error?.original?.stack as stack, i}
 			{#if i > 0}
-				<li>{stack}</li>
+				<li class:internal={INTERNAL.test(stack)}>{stack}</li>
 			{/if}
 		{/each}
 	</ol>
@@ -17,5 +19,8 @@
 <style>
 	.stack li {
 		margin: 0.5em 0;
+	}
+	.stack li.internal {
+		color: color-mix(in srgb, currentColor 25%, Canvas);
 	}
 </style>
